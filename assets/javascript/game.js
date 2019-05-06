16f7742f63974934dbb5755dec7bdd7fec790278
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-05 19:04:21 -0500

    added html and psuedocoded js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//initial variables
+
+var name = "tim"
+
+
+
+// needs to register key press to initiate program
+
+
+// needs to select words from array of words
+    //count the number of letters 
+    //display blank spaces equal to number of letters
+
+
+//display win and lose counter
+//display guess counter
+
+//track user key strokes and compare letters in choosen word to user choices 
+    //if user input matches chosen word's letters, display letter in appropriate blank 
+    //if user input doesn't match chosen word's letters, subtract one from guess counter. 
+
+//if all the letters in chosen word are selected, add one to win counter and restart. 
+//if all the guesses are used, add one to lose counter and restart. 
