21f28f0e5a170b3b9c155c035ab4d59303476696
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-05 20:15:11 -0500

    created function to select random word from array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,21 @@
-//initial variables
-
-var spaceWords = ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"]
-
-
-
-
-
 // needs to register key press to initiate program
 
+//initial object
 
+
+
+var wordGame = {
+    guess: 12,
+    loss: 0,
+    win: 0,
+    spaceWords: ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"],
+
+    randWord: function() {
+       return wordGame.spaceWords[Math.floor(Math.random() * wordGame.spaceWords.length)]
+    }
 // needs to select words from array of words
     //count the number of letters 
     //display blank spaces equal to number of letters
-
 
 //display win and lose counter
 //display guess counter
@@ -23,3 +26,16 @@ var spaceWords = ["moon","star","event horizon","graviton","black hole", "corona
 
 //if all the letters in chosen word are selected, add one to win counter and restart. 
 //if all the guesses are used, add one to lose counter and restart. 
+
+
+}
+
+
+
+
+
+
+
+
+
+
