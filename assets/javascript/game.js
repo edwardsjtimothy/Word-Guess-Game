1c512b68ecd8327ee48220618d47272ca066a963
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-05 23:28:49 -0500

    tinkering

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,25 @@
 
 
 
-var wordGame = {
+let wordGame = {
     guess: 12,
     loss: 0,
     win: 0,
     spaceWords: ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"],
 
+   //selects random word
+   
     randWord: function() {
-       return wordGame.spaceWords[Math.floor(Math.random() * wordGame.spaceWords.length)]
+      return this.spaceWords[Math.floor(Math.random() * this.spaceWords.length)];
+    },
+
+
+    //seperates selected word into array of its letters 
+    
+    randWordArray: function() {
+        return this.randWord.split("");
     }
-// needs to select words from array of words
+
     //count the number of letters 
     //display blank spaces equal to number of letters
 
@@ -27,15 +36,8 @@ var wordGame = {
 //if all the letters in chosen word are selected, add one to win counter and restart. 
 //if all the guesses are used, add one to lose counter and restart. 
 
-
 }
 
-
-
-
-
-
-
-
+// var randWordArray = wordGame.randWord.split(" ");
 
 
