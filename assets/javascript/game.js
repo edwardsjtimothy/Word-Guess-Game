9e9db11a329b3d982537816c5c57ee746b026695
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-07 10:39:06 -0500

    abandoning method approach and starting over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,36 @@
 // needs to register key press to initiate program
 
-//initial object
+//initial variables
 
+let spaceWords = ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"];
 
-
-let wordGame = {
-    guess: 12,
-    loss: 0,
-    win: 0,
-    spaceWords: ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"],
-
-   //selects random word
+//selects random word
    
-    randWord: function() {
-      return this.spaceWords[Math.floor(Math.random() * this.spaceWords.length)];
-    },
+let randomSpaceWord = spaceWords[Math.floor(Math.random() * this.spaceWords.length)];
+  
+let s;
+let wins = 0;
+let loses = 0;
+let letterGuesses = [];
+ 
+function gameStart() {
 
-    //seperates selected word into array of its letters 
+  for (var i = 0; i < randomSpaceWord.length; i++)
+  {
+    randomSpaceWord[i] = "_";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+//seperates selected word into array of its letters 
 
     randWordArray: function() {
       wordGame.randWord().split("");
