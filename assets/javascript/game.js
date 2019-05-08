a53cc55dcedcaa457016439a6cdadbcbf43563ed
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-08 12:13:11 -0500

    added functions for rudamentary counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,29 +6,30 @@
 var wordGame = {
 wins: 0,
 losses: 0,
-guesses: 0,
+guesses: 12,
 spaceWords: ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"],
 lettersArray: [],
+
 //selects random word
    
 randSpaceWord: function() {
   return this.spaceWords[Math.floor(Math.random() * this.spaceWords.length)];
 },
 
-
-
 // splits chosen word into individual strings for each letter and space
+
 spaceWordArray: function () {
   
- lettersArray = Array.from(wordGame.randSpaceWord()); //split method does not seem to work. What is the correct sytax for use in object?
+ lettersArray = Array.from(wordGame.randSpaceWord()); 
   
-// creates a blank for each letter and space in array?
+    // creates a blank for each letter and space in array?
 
   for (var i = 0; i < wordGame.randSpaceWord.length; i++) 
   {
     this.lettersArray[i] = "_";
   } 
 },
+
 
 // identfies and stores user keystrokes
 
@@ -48,3 +49,29 @@ spaceWordArray: function () {
 
 }
 
+
+// calls function to generate random word 
+
+wordGame.randSpaceWord();
+wordGame.spaceWordArray();
+
+//wins counter
+
+var winsCounter = document.getElementById("wins") 
+  winsCounter.innerHTML = wordGame.wins; 
+
+// losses counter 
+
+var lossesCounter = document.getElementById("losses") 
+  lossesCounter.innerHTML = wordGame.losses; 
+
+// guesses remaining counter
+
+var guessesCounter = document.getElementById("guesses") 
+  guessesCounter.innerHTML = wordGame.guesses; 
+
+
+//chosen word 
+
+var chosenWord = document.getElementById("word") 
+  chosenWord.innerHTML = wordGame.lettersArray;
