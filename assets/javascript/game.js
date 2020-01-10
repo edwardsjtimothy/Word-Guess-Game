729d72bbdbfbf1904d717f90820a67b3628ad01f
Enragedtoast <edwardsjtimothy@gmail.com>
2020-01-10 10:34:00 -0600

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,48 @@
-// spent too long trying to make this work. In retrospect, I should have switched to the basic assignment so I could have turned in something functional. Live and learn! Still, learned a lot trying to get it running so I feel ok about it. 
 
-// initial object 
-var wordGame = {
-wins: 0,
-losses: 0,
-guesses: 12,
-spaceWords: ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"],
-lettersArray: [],
-blanksArray: [],
-match: false,
-userGuess: [],
+const wins = 0;
+const losses = 0;
+const guesses = 12;
+const spaceWords = ["moon","star","event horizon","graviton","black hole", "coronal mass ejection", "olympus mons", "nasa","spirit", "opportunity","edwin hubble","alpha centauri","neutron star","singularity","jpl","oumuamua","apollo","buzz aldrin","project starshot","space x","esa","yutu","seti","neil armstrong","challenger","oort cloud","large magellanic cloud","hubble deep field","mars","venus","jupiter","uranus","neptune","mercury","kuiper belt","neo","meteor","comet","kepler space telescope","quasar","sun spot","exoplanets","parsec","pluto","earth","iss","sea of tranquility","io","ganymede","europa","titan","callisto","relativity","stephen hawking","hawking radiation","accretion disk","pulsar","white dwarf","brown dwarf","m class","sagittarius a","super nova","betelgeuse","orion","sirius","arcturus"];
+const lettersArray = [];
+const blanksArray = [];
+const match = false;
+const userGuess = [];
+
+//wins counter
+document.getElementById("wins").innerHTML = wins;
+
+// losses counter 
+document.getElementById("losses").innerHTML = losses;
+
+// guesses remaining counter
+document.getElementById("guesses").innerHTML = guesses;
+
+//chosen word 
+document.getElementById("word").innerHTML = ; //should be blanksArray but does seem to push. will return undefined.
 
 
-
-gameStart: function () { //how do I call it onkeyup? tried document.onkeyup(wordGame.gameStart()); doesn't work
+gameStart =()=> {//how do I call it onkeyup? tried document.onkeyup(wordGame.gameStart()); doesn't work
   wordGame.randSpaceWord();  
   wordGame.spaceWordArray();
   wordGame.blanks();
-},
+}
 
 //selects random word
    
-randSpaceWord: function() {
+randSpaceWord =()=> {
   let arrPos = Math.floor(Math.random() * this.spaceWords.length);
   return this.spaceWords[arrPos];
 },
 
 // splits chosen word into individual strings for each letter and space
 
-spaceWordArray: function () {
-  
+spaceWordArray =()=> {
 lettersArray = Array.from(wordGame.randSpaceWord()); 
 },
 
 //generates blanks for each letter in random word 
 
-blanks: function () {
+blanks =()=> {
 
  for (var i = 0; i < wordGame.spaceWordArray.length; i++)  //for each element in word array, push _ to blanks array 
   {
@@ -50,16 +57,15 @@ blanks: function () {
  
 // displays list of user key strokes. Stores user keystrokes in variable.
 
-input: function () {
-     
-   var userKeys = document.getElementById("keylog").value;
+input =()=> {
+   var userKeys = e.key
   document.getElementById("keyCollector").innerHTML = userKeys; // I was unable to track keystrokes without requiring an input field. Tried variations of document.onkeyup to no avail. 
 },
 
 
 // compares user keystrokes with letters stored in lettersArray
     
-guessCheck: function () {
+guessCheck =()=> {
   // wordGame.userGuess = document.getElementById("keylog").value;
   wordGame.secretWord = document.getElementById("word");
   for (var i = 0; i < wordGame.secretWord.length; i++) {
@@ -76,7 +82,7 @@ guessCheck: function () {
     }
   // if all letters from lettersArray are chosen by user then add one to wins and restart
 },
-}
+
 
 // calls functions to run game
 // document.onkeyup(wordGame.gameStart()); doesn't work :(
@@ -84,18 +90,3 @@ guessCheck: function () {
 wordGame.gameStart();
 
 
-//wins counter
-const winsCounter = document.getElementById("wins"); 
-  winsCounter.innerHTML = wordGame.wins; 
-
-// losses counter 
-const lossesCounter = document.getElementById("losses"); 
-  lossesCounter.innerHTML = wordGame.losses; 
-
-// guesses remaining counter
-const guessesCounter = document.getElementById("guesses"); 
-  guessesCounter.innerHTML = wordGame.guesses; 
-
-//chosen word 
-const chosenWord = document.getElementById("word"); 
-  chosenWord.innerHTML = this.lettersArray; //should be blanksArray but does seem to push. will return undefined.
